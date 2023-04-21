3ddd02f419f180e90d6d76584545e1bf6ffd3336
Matt Lin <matt.lin@gamesofa.com>
2023-04-21 11:51:43 +0800

    1. 新增 fullscreen 2. 新增 favicon

--- script.js
+++ script.js
@@ -51,5 +51,40 @@ function animate() {
 	requestAnimationFrame(animate);
 }
 
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+	console.info("openFullscreen")
+  if (elem.requestFullscreen) {
+	console.info("requestFullscreen")
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+
 init();
 animate();
+
+// Add event listener on keydown
+document.addEventListener('keydown', (event) => {
+	const code = event.code;
+	if(event.code === 'KeyF') {
+		openFullscreen();
+	} 
+}, false);
